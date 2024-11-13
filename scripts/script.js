9f04781af18ab00ef189c9a94e960858ec77f940
user's <macbook@drep.local>
2024-11-14 03:31:06 +0700

    add code in js dan h1 di html

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+var cityInput = document.getElementById("searchCity")
+
+var backgroundList = [
+
+    "day1.jpg"
+    "day2.jpg"
+    "day3.jpg"
+    "day4.jpg"
+    "day5.jpg"
+
+];
+
+var randomBackground = backgroundsList[Math.floor(Math.random() * backgroundsList.length)];
